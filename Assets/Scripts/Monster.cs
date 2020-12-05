045ecce006a95cb30163eb011c40bdd752a83df1
xXHellgate25Xx <55650626+xXHellgate25Xx@users.noreply.github.com>
2020-12-05 15:31:48 +0700

    Update Monster.cs

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Monster : LiveObject
 {
+    public Transform[] path;
+    public Transform monster;
+
+    private Vector2 position;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (path.Length == 0)
+        {
+            Debug.LogError("No path referenced.");
+        }
+        position = gameObject.transform.position;
     }
 
     // Update is called once per frame
